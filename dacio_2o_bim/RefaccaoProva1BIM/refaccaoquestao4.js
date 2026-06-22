7f0f2daf8eb1d4602901c17c2d5b15860c7c8d2d
jvos15032006-wq <jvos15032006@gmail.com>
2026-06-21 21:11:28 -0300

    Create refaccaoquestao4.js

---
+++ dacio_2o_bim/RefaccaoProva1BIM/refaccaoquestao4.js
@@ -0,0 +1,15 @@
+function revelar() {
+  document.getElementById("imagemJogador").src = "vini.png";
+
+  document.getElementById("nome").textContent = "Vinícius José Paixão de Oliveira Júnior";
+  document.getElementById("nascimento").textContent = "Data de Nascimento: 12/07/2000 (25 anos)";
+  document.getElementById("altura").textContent = "Altura: 1,76 m";
+  document.getElementById("posicao").textContent = "Posição: Ponta-esquerda / Atacante";
+  document.getElementById("rank").textContent = "Rank: 9,5";
+
+  var elementos = document.querySelectorAll(".placeholder");
+  elementos.forEach(function(el) {
+    el.classList.remove("placeholder");
+    el.classList.add("card-text");
+  });
+}
